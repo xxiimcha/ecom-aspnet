c0fe84557fbc371a0ac477bef052bcfe8b2fe91e
Charmaine Cator <xxiimcha@users.noreply.github.com>
2024-11-25 10:58:32 +0800

    Fixed the error

--- Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d9914508e7da02bb195bb53863096958eca5b04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04645cda5a243e3405a31d80725f0ddddce3bc25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
